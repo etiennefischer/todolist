03fe92f92838933318f88946d0a761b369c1b106
Pierre-Alban Toth <pat@clearwage.com>
2021-03-21 20:59:55 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ['./index.html', './src/**/*.{vue,js}'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
