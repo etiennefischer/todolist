1da8c2281d3fdb02ff6eedd5ee84ec266d0bd452
Etienne <fischer.etienne@gmail.com>
2022-02-14 20:42:41 +0100

    Update firebase config

--- src/store/api.js
+++ src/store/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://todolist-6bc89-default-rtdb.europe-west1.firebasedatabase.app'
+  baseURL: 'https://vue-todolist-etienne-default-rtdb.europe-west1.firebasedatabase.app'
 })
